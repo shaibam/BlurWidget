c1113b8e1a49f10ff7040db4ccf53566590ef565
shaibam <shaibam@gmail.com>
2018-03-11 01:57:16 +0200

    before changes are applied to canvas

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-((_imageContainer)=>{
+((_imageContainer, button) => {
     var _focusElement = _imageContainer.querySelector('div.focus');
     var _blurElement = _imageContainer.querySelector('div.blur');
-    _imageContainer.onmousemove= (e)=>{
-        _focusElement.style.setProperty('clip-path', 'circle(60px at '+ (e.screenX-30)+'px '+(e.screenY-30)+'px)');
+    _imageContainer.onmousemove = (e) => {
+        _focusElement.style.setProperty('clip-path', 'circle(60px at ' + (e.screenX - 30) + 'px ' + (e.screenY - 30) + 'px)');
     }
 
-})( document.querySelector('div.image-container'))
+    button.onclick = () => {
+       
+    }
+
+})(document.querySelector('div.image-container'), document.querySelector('button'))
