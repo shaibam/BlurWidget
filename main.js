a58cca17e780fa8cf8e4a816493aa1d8bdb0d20c
shaibam <shaibam@gmail.com>
2018-03-11 10:08:58 +0200

    code cleanup

--- main.js
+++ main.js
@@ -1,37 +1,46 @@
-((_imageContainer, button) => {
-    var _focusElement = _imageContainer.querySelector('div.focus');
-    var _blurElement = _imageContainer.querySelector('div.blur');
-    _imageContainer.onmousemove = (e) => {
-        _focusElement.style.setProperty('clip-path', 'circle(60px at ' + (e.screenX - 30) + 'px ' + (e.screenY - 30) + 'px)');
+//Create a stage by getting a reference to the canvas
+var image = new Image();
+image.src = 'fela.jpg';
+image.onload = () => {
+    var circleAlpha;
+    var maskedImage;
+    var stage = new createjs.Stage("canvas"); //select stage elemnt
+    var bitmap = new createjs.Bitmap(image);
+    var blurFilter = new createjs.BlurFilter(10,10,1); //blur image
+
+    var maskImage=(x,y)=>{
+        //apply a clear circle mask according to X,Y coordinates
+        circleAlpha = new createjs.Shape();
+        circleAlpha.graphics.beginFill("#000000")
+        circleAlpha.graphics.drawCircle(x ,y, 50);
+        circleAlpha.cache(0, 0,image.width,image.height);        
+        maskedImage.filters = [
+            new createjs.AlphaMaskFilter(circleAlpha.cacheCanvas)
+        ];
+        maskedImage.cache(0,0,image.width,image.height);
+        stage.update();
+    }
+    // blur image
+    bitmap.filters = [blurFilter];
+    bitmap.cache(0,0,image.width,image.height);
+    stage.addChild(bitmap);
+
+    maskedImage = new createjs.Bitmap(image); //create a duplicate of the image to be masked
+    stage.addChild(maskedImage);
+    maskImage(-50,-50)
+
+    document.onmousemove = (e) => {
+        maskImage(e.clientX ,e.clientY);
     }
 
-
-    var img = new Image();
-    img.src = 'fela.jpg';
-    var canvasElement = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    ctx.filter = 'blur(5px)';
-    img.onload = function () {
-        ctx.drawImage(img, 0, 0);
-        img.style.display = 'none';
-    };
-
-    button.onclick = () => {
-        html2canvas(document.body).then(canvas => {
-            document.body.appendChild(canvas)
-        });
+    var canvas = document.querySelector('#canvas')
+    canvas.onclick = () => {
+        // clone image and create a 'a' elment to download
+        var imgClone = canvas.toDataURL('image/png');
+        var ahref=document.createElement('a');
+        ahref.innerHTML = 'Download image'
+        ahref.href=imgClone;
+        ahref.download=true;
+        document.body.appendChild(ahref)
     }
-    var zoomctx = document.getElementById('zoom').getContext('2d');
-    var zoom = function (event) {
-        var x = event.layerX;
-        var y = event.layerY;
-        zoomctx.drawImage(canvas,
-            Math.min(Math.max(0, x - 5), img.width - 10),
-            Math.min(Math.max(0, y - 5), img.height - 10),
-            10, 10,
-            0, 0,
-            200, 200);
-    };
-    canvas.addEventListener('mousemove', zoom); 
-
-})(document.querySelector('div.image-container'), document.querySelector('button'))
+}
